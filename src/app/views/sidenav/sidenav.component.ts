1e92d55dcf36496a854fbccddb1fe69c5ccca287
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-12-02 15:07:14 +0100

    layout implementation

--- src/app/views/sidenav/sidenav.component.ts
+++ src/app/views/sidenav/sidenav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
+  private _mobileQueryListener: () => void;
 
-  ngOnInit(): void {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnInit(): void {}
 }
