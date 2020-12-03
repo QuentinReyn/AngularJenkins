749b83390fa0841d09e0c0fbab7c975229e62292
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-12-03 16:17:01 +0100

    removed phantomJS / add headlessChrome

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,12 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      // require('karma-chrome-launcher'),  /*comment out this line to disable the karma-chrome-launcher*/
-      require('karma-phantomjs-launcher'),  /* add this line to disable the karma-phantomjs-launcher*/
-      require('karma-jasmine-html-reporter'),
+      require('karma-chrome-launcher'),  /*comment out this line to disable the karma-chrome-launcher*/
+      //require('karma-phantomjs-launcher'),  /* add this line to disable the karma-phantomjs-launcher*/
+      //require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
       require('karma-coverage'),
+      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -27,12 +29,23 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha','coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: true
   });
