3e796f74745a71ee0ae71fd50a3638503de7588b
QuentinReynaud <quentin.reynaud99@gmail.com>
2021-02-10 09:45:59 +0100

    fix service tests

--- src/app/services/choice-user.service.spec.ts
+++ src/app/services/choice-user.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ChoiceUserUserService } from './choice-user.service';
@@ -6,7 +7,9 @@ describe('ChoiceUserService', () => {
   let service: ChoiceUserUserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ChoiceUserUserService);
   });
 
--- src/app/services/choice.service.spec.ts
+++ src/app/services/choice.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ChoiceService } from './choice.service';
@@ -6,7 +7,9 @@ describe('ChoiceService', () => {
   let service: ChoiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ChoiceService);
   });
 
