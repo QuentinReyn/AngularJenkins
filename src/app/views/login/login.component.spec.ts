2992318d5cc0ad4f88358a7a0c4bb76fd77102d7
QuentinReynaud <quentin.reynaud99@gmail.com>
2021-02-10 09:59:31 +0100

    fix login tests

--- src/app/views/login/login.component.spec.ts
+++ src/app/views/login/login.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +12,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent],
+      imports: [HttpClientModule, RouterModule.forRoot([]),BrowserModule,FormsModule, ReactiveFormsModule],
+      //providers: []
     })
     .compileComponents();
   });
